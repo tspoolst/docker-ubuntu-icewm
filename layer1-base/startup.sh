19bacd7a1aba7114629e4ba9bd9ee95306eb2aa3
Trenton Spoolstra <Trenton.H.Spoolstra@kp.org>
2018-09-30 17:44:13 -0700

    added more layer controls, dev apps, and fixed bash-completion apt-get

--- layer1-base/startup.sh
+++ layer1-base/startup.sh
@@ -9,5 +9,9 @@ else
         #chown -R ka-lite:nogroup /var/ka-lite/.kalite
         #service ka-lite start
         update-locale
+        ##removing docker-clean allows bash-completion to work with apt-get package searches.
+        rm -fv /etc/apt/apt.conf.d/docker-clean
+        apt-get update
+        [[ -e /usr/bin/apt-file ]] && apt-file update
         date > /etc/configured
 fi
